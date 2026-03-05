d4a012570afdafd764b06cecc14237b54a900f92
prashantsinghjadaun <jadaunprashant777@gmail.com>
2026-03-06 00:38:04 +0530

    Added New Arrivals feature using vector indexing

--- main.cpp
+++ main.cpp
@@ -121,19 +121,43 @@ void returnBook(){
 
     cout<<"Book not found\n";
 }
+void showRecentBooks(){
+
+    if(library.empty()){
+        cout<<"No books in library\n";
+        return;
+    }
+
+    cout<<"\nNew Arrivals\n";
+    cout<<"--------------------\n";
+
+    int start = max(0, (int)library.size() - 3);
+
+    for(int i = start; i < library.size(); i++){
+        cout<<"ID: "<<library[i].id<<" ";
+        cout<<"Title: "<<library[i].title<<endl;
+    }
+}
 
 int main(){
 
     int choice;
 
+    cout<<"=================================\n";
+    cout<<"      Library Management System\n";
+    cout<<"=================================\n";
+
     while(true){
+
+        cout<<"\n";
 
         cout<<"1 Add Book\n";
         cout<<"2 Show Books\n";
         cout<<"3 Search Book\n";
         cout<<"4 Issue Book\n";
         cout<<"5 Return Book\n";
-        cout<<"6 Exit\n";
+        cout<<"6 New Arrivals\n";
+        cout<<"7 Exit\n";
 
         cout<<"Enter choice: ";
         cin>>choice;
@@ -154,6 +178,9 @@ else if(choice==5)
     returnBook();
 
 else if(choice==6)
+    showRecentBooks();
+
+else if(choice==7)
     break;
 
         else
